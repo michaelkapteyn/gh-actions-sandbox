83e45abb0ca9f25aee34398c130ce00a5bd905dd
Michael Kapteyn <michaelkapteyn@gmail.com>
2024-05-16 14:04:21 -0700

    add some dummy code

---
+++ some_code.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def my_func():
+    return 7.
+
+def my_other_func():
+    return np.zeros(2,2)
+
+
---
+++ tests/test_funcs.py
@@ -0,0 +1,8 @@
+from some_code import my_func, my_other_func
+
+def test_my_func():
+    assert my_func() == 7 # should pass!
+
+def test_my_other_func():
+    assert my_other_func()[0,0] == 1. # should fail!
+    
