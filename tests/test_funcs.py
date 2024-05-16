36cfd87806f0e3a1b9d7671b005ce0b32f050cb7
Michael Kapteyn <michaelkapteyn@gmail.com>
2024-05-16 14:33:15 -0700

    format

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,8 +1,9 @@
 from some_code import my_func, my_other_func
 
+
 def test_my_func():
-    assert my_func() == 7 # should pass!
+    assert my_func() == 7  # should pass!
+
 
 def test_my_other_func():
-    assert my_other_func()[0,0] == 1. # should fail!
-    
+    assert my_other_func()[0, 0] == 1.0  # should fail!
